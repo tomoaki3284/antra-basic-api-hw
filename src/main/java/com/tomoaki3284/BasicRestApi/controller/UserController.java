7c5b41b7346625d6cbc7388d97ae567cb875525d
Tomoaki Mitsuhashi <tomoaki3284@gmail.com>
2022-05-26 11:27:38 -0400

    change path

--- src/main/java/com/tomoaki3284/BasicRestApi/controller/UserController.java
+++ src/main/java/com/tomoaki3284/BasicRestApi/controller/UserController.java
@@ -8,15 +8,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/v1/api")
 public class UserController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
